3492043a4a697275a8f554acda57cb4980cef945
supertgo <thiagoroberto2003@gmail.com>
2022-02-05 08:20:35 -0300

    feat(api): :sparkles: saving events webhook data

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,71 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { NextApiRequest, NextApiResponse } from 'next';
+import { stripe } from 'services/stripe';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { saveSubscription } from './_lib/manageSubscription';
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+const relevantEvents = new Set(['checkout.session.completed']);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('Evento recebido');
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const secret = req.headers['stripe-signature'];
 
-  res.status(200).json({ ok: true });
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret!,
+        process.env.STRIPE_WEBHOOK_SECRET!
+      );
+    } catch (error) {
+      return res.status(400).send(`Webhook error: ${error.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            // eslint-disable-next-line no-case-declarations
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription!.toString(),
+              checkoutSession.customer!.toString()
+            );
+            break;
+          default:
+            throw new Error('Unhandled event.');
+        }
+      } catch (error) {
+        return res.json({ error: 'Webhook handler failed.' });
+      }
+    }
+
+    res.json({ received: true });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed');
+  }
 };
