9163817091f02f68cab93b6d4ec41d714035d115
supertgo <thiagoroberto2003@gmail.com>
2022-02-08 09:08:15 -0300

    feat(api): :sparkles: consuming prismic api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,25 @@
+import { getPrismicClient } from 'services/prismic';
 import Posts from 'templates/Posts';
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
 
 export default function PostsPage() {
   return <Posts />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100
+    }
+  );
+
+  console.log(response);
+
+  return {
+    props: {}
+  };
+};
---
+++ src/services/prismic.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import Prismic from '@prismicio/client';
+
+export function getPrismicClient(req?: unknown) {
+  const prismic = Prismic.client(process.env.PRISMIC_ENDPOINT!, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  });
+
+  return prismic;
+}
